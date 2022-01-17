bd2e8a89c7232f1071f7ed8ffe819dd55b5a4f66
HashemJaafar <84957709+HashemJaafar@users.noreply.github.com>
2022-01-17 21:35:47 +0300

    Add files via upload

--- algebra.go
+++ algebra.go
@@ -24,7 +24,7 @@ func EQUATIONS_SOLVER(print, check_if_keys_in_the_equations bool, m map[string]f
 	if check_if_keys_in_the_equations {
 		check_map_keys_for_equations(equations, m)
 	}
-	for a := 0; a <= len(equations); a++ {
+	for a := 0; a <= len(equations)+1; a++ {
 		for _, equation := range equations {
 			equation_solver(print, m, equation[0], equation[1], equation[2], equation[3])
 		}
@@ -32,18 +32,9 @@ func EQUATIONS_SOLVER(print, check_if_keys_in_the_equations bool, m map[string]f
 }
 
 func equation_solver(print bool, m map[string]float64, a, b, sign, c string) {
-	la, erra := strconv.ParseFloat(a, 64)
-	lb, errb := strconv.ParseFloat(b, 64)
-	lc, errc := strconv.ParseFloat(c, 64)
-	if erra == nil {
-		m[a] = la
-	}
-	if errb == nil {
-		m[b] = lb
-	}
-	if errc == nil {
-		m[c] = lc
-	}
+	assign_number_if_number(m, a)
+	assign_number_if_number(m, b)
+	assign_number_if_number(m, c)
 	switch sign {
 	case "+":
 		equations_generator(print, m, a, b, sign, c, m[b]+m[c], m[a]-m[c], m[a]-m[b])
@@ -64,28 +55,23 @@ func equation_solver(print bool, m map[string]float64, a, b, sign, c string) {
 	}
 }
 
+func assign_number_if_number(m map[string]float64, str string) {
+	number, err := strconv.ParseFloat(str, 64)
+	if err == nil {
+		m[str] = number
+	}
+}
+
 func equations_generator(print bool, m map[string]float64, a, b, sign, c string, a_value, b_value, c_value float64) {
 	la, oka := m[a]
 	lb, okb := m[b]
 	lc, okc := m[c]
-	var inf bool
-	for _, a := range []float64{m[a], m[b], m[c]} {
-		if math.IsInf(a, 0) {
-			inf = true
-		}
-	}
-	if math.IsNaN(la) {
-		m[a] = 0
-	}
-	if math.IsNaN(lb) {
-		m[b] = 0
-	}
-	if math.IsNaN(lc) {
-		m[c] = 0
-	}
-	if math.IsNaN(a_value) {
-		a_value = 0
-	}
+	convert_nan_to_zero_for_map(m, a, la)
+	convert_nan_to_zero_for_map(m, b, lb)
+	convert_nan_to_zero_for_map(m, c, lc)
+	a_value = convert_nan_to_zero(a_value)
+	b_value = convert_nan_to_zero(b_value)
+	c_value = convert_nan_to_zero(c_value)
 	switch {
 	case !oka && okb && okc:
 		m[a] = a_value
@@ -96,8 +82,30 @@ func equations_generator(print bool, m map[string]float64, a, b, sign, c string,
 	case oka && okb && !okc:
 		m[c] = c_value
 		print_equation(print, m, a, b, sign, c)
-	case oka && okb && okc && math.Round(la*1000)/1000 != math.Round(a_value*1000)/1000 && !inf:
+	case oka && okb && okc && math.Round(la*1000)/1000 != math.Round(a_value*1000)/1000 && !check_if_inf(m, a, b, c):
 		log.Fatal(a, " ", m[a], " != ", b, " ", m[b], " ", sign, " ", c, " ", m[c])
+	}
+}
+
+func check_if_inf(m map[string]float64, a string, b string, c string) bool {
+	for _, a := range []float64{m[a], m[b], m[c]} {
+		if math.IsInf(a, 0) {
+			return true
+		}
+	}
+	return false
+}
+
+func convert_nan_to_zero(value float64) float64 {
+	if math.IsNaN(value) {
+		value = 0
+	}
+	return value
+}
+
+func convert_nan_to_zero_for_map(m map[string]float64, str string, value float64) {
+	if math.IsNaN(value) {
+		m[str] = 0
 	}
 }
 
